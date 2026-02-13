8cde8fc012995f96e346ca4785b2498a05485617
narvaezlluscomedbelizario-coder <narvaezlluscomedbelizario@gmail.com>
2026-02-13 03:18:22 -0400

    Update configuration.php

--- configuration.php
+++ configuration.php
@@ -1,19 +1,11 @@
 <?php
 session_start();
-
-/** Xtream Host (tu DNS/URL) */
 $XCStreamHostUrl = "http://redworld.pro:8880";
-
-/** Branding / links */
-$XClogoLinkval        = "images/logo.png";
-$XCcopyrighttextval   = "";
-$XCcontactUslinkval   = "";
-$XChelpLinkval        = "";      // OJO: es help, no "hepl"
-
-/** Licencia (solo para que NO redirija a install) */
-$XClicenseIsval = "Snulled8";    // O cualquier texto NO vacío
-$XClocalKey     = "x";           // O cualquier texto NO vacío
-
-/** Título */
-$XCsitetitleval = "IPTV Smarters WebTV";
+$XClogoLinkval = "images/logo.png";
+$XCcopyrighttextval = "";
+$XCcontactUslinkval = "";
+$XChelpLinkval = "";
+$XClicenseIsval = "8nulled8";
+$XClocalKey = "railway";
+$XCsitetitleval = "IPTV Panel";
 ?>
